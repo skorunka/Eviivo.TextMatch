bd6c84430198fc817b48525df5ebd67f065611d1
Frantisek Skorunka <skorunka@outlook.com>
2015-07-29 13:45:06 +0100

    partial implementation of StringUtilities service.

---
+++ TextMatch.Services/StringUtilities.cs
@@ -0,0 +1,35 @@
+﻿namespace TextMatch.Services
+{
+	using System.Collections;
+	using System.Collections.Generic;
+
+	public static class StringUtilities
+	{
+		public static ICollection<int> GetSubtextPositions(string text, string subText)
+		{
+			var sub = subText.ToCharArray();
+
+			var positions = new List<int>(sub.Length);
+
+			for (var i = 0; i < text.Length; i++)
+			{
+				var found = true;
+				for (var j = 0; j < sub.Length; j++)
+				{
+					if (text[i + j] != sub[j])
+					{
+						found = false;
+						break;
+					}
+				}
+
+				if (found)
+				{
+					positions.Add(i);
+				}
+			}
+
+			return positions;
+		}
+	}
+}
